870fcc39110d3bc382ddb4080f770327f6e9e7f9
Yang Kyowon <seolyang.contact@gmail.com>
2023-10-17 22:39:59 +0900

    Add timer

---
+++ Source/Frieren/Core/Timer.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <chrono>
+
+namespace fe
+{
+	class Timer final
+	{
+	public:
+		Timer() = default;
+		Timer(const Timer&) = delete;
+		Timer(Timer&&) = delete;
+		~Timer() = default;
+
+		Timer& operator=(const Timer&) = delete;
+		Timer& operator=(Timer&&) = delete;
+
+		void Begin()
+		{
+			begin = std::chrono::high_resolution_clock::now();
+		}
+
+		void End()
+		{
+			deltaTime = std::chrono::high_resolution_clock::now() - begin;
+		}
+
+		[[nodiscard]] uint64_t GetDeltaTimeNanos() const
+		{
+			return deltaTime.count();
+		}
+
+		[[nodiscard]] uint64_t GetDeltaTimeMillis() const
+		{
+			return std::chrono::duration_cast<std::chrono::milliseconds>(deltaTime).count();
+		}
+
+		[[nodiscard]] double GetDeltaTimeF64() const
+		{
+			return GetDeltaTimeNanos() * 1e-09;
+		}
+
+		[[nodiscard]] float GetDeltaTime() const
+		{
+			return static_cast<float>(GetDeltaTimeF64());
+		}
+
+	private:
+		std::chrono::steady_clock::time_point begin = std::chrono::high_resolution_clock::now();
+		std::chrono::nanoseconds			  deltaTime = std::chrono::nanoseconds(0);
+	};
+} // namespace fe
