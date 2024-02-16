faf72c249ef6ee0ea98e8356b3696905d5a5def9
Yang Kyowon <seolyang.contact@gmail.com>
2024-02-17 00:42:29 +0900

    Test code upload buffer

--- Source/Frieren/Renderer/Renderer.cpp
+++ Source/Frieren/Renderer/Renderer.cpp
@@ -33,6 +33,7 @@ namespace fe
 				std::make_unique<dx::Fence>(device.CreateFence(std::format("FrameFence_{}", localFrameIdx)).value()));
 		}
 
+		// #test #todo upload to buffer/texture
 		dx::GPUBufferDesc vbDesc{};
 		vbDesc.AsVertexBuffer<SimpleVertex>(4);
 		quadVB = std::make_unique<dx::GPUBuffer>(renderDevice.CreateBuffer(vbDesc).value());
@@ -40,6 +41,73 @@ namespace fe
 		dx::GPUBufferDesc ibDesc{};
 		ibDesc.AsIndexBuffer<uint16_t>(6);
 		quadIB = std::make_unique<dx::GPUBuffer>(renderDevice.CreateBuffer(ibDesc).value());
+
+		dx::Fence		   uploadFence = device.CreateFence("UploadFence").value();
+		dx::CommandContext uploadCtx = device.CreateCommandContext(dx::EQueueType::Direct).value();
+
+		dx::GPUBufferDesc quadUploadDesc{};
+		quadUploadDesc.AsUploadBuffer(static_cast<uint32_t>(vbDesc.GetSizeAsBytes() + ibDesc.GetSizeAsBytes()));
+		dx::GPUBuffer	   quadUploadBuffer = renderDevice.CreateBuffer(quadUploadDesc).value();
+		const SimpleVertex vertices[4] = {
+			{ -.5f, 0.f, 0.f },
+			{ -0.5f, 0.5f, 0.f },
+			{ .5f, 0.5f, 0.f },
+			{ .5f, 0.f, 0.f }
+		};
+
+		const uint16_t indices[6] = {
+			0,
+			1,
+			2,
+			2,
+			1,
+			3
+		};
+
+		{
+			dx::GPUResourceMapGuard mappedUploadBuffer = quadUploadBuffer.Map();
+			std::memcpy(mappedUploadBuffer.get(), vertices, vbDesc.GetSizeAsBytes());
+			std::memcpy(mappedUploadBuffer.get() + vbDesc.GetSizeAsBytes(), indices, ibDesc.GetSizeAsBytes());
+		}
+
+		uploadCtx.Begin();
+		uploadCtx.AddPendingBufferBarrier(
+			*quadVB,
+			D3D12_BARRIER_SYNC_NONE, D3D12_BARRIER_SYNC_COPY,
+			D3D12_BARRIER_ACCESS_NO_ACCESS, D3D12_BARRIER_ACCESS_COPY_DEST);
+
+		uploadCtx.AddPendingBufferBarrier(
+			*quadIB,
+			D3D12_BARRIER_SYNC_NONE, D3D12_BARRIER_SYNC_COPY,
+			D3D12_BARRIER_ACCESS_NO_ACCESS, D3D12_BARRIER_ACCESS_COPY_DEST);
+
+		uploadCtx.AddPendingBufferBarrier(
+			quadUploadBuffer,
+			D3D12_BARRIER_SYNC_NONE, D3D12_BARRIER_SYNC_COPY,
+			D3D12_BARRIER_ACCESS_NO_ACCESS, D3D12_BARRIER_ACCESS_COPY_SOURCE);
+
+		uploadCtx.FlushPendingBufferBarriers();
+
+		uploadCtx.CopyBuffer(quadUploadBuffer, 0, vbDesc.GetSizeAsBytes(), *quadVB, 0);
+		uploadCtx.CopyBuffer(quadUploadBuffer, vbDesc.GetSizeAsBytes(), ibDesc.GetSizeAsBytes(), *quadIB, 0);
+
+		uploadCtx.AddPendingBufferBarrier(
+			*quadVB,
+			D3D12_BARRIER_SYNC_COPY, D3D12_BARRIER_SYNC_VERTEX_SHADING,
+			D3D12_BARRIER_ACCESS_NO_ACCESS, D3D12_BARRIER_ACCESS_VERTEX_BUFFER);
+
+		uploadCtx.AddPendingBufferBarrier(
+			*quadIB,
+			D3D12_BARRIER_SYNC_COPY, D3D12_BARRIER_SYNC_INDEX_INPUT,
+			D3D12_BARRIER_ACCESS_COPY_DEST, D3D12_BARRIER_ACCESS_INDEX_BUFFER);
+
+		uploadCtx.FlushPendingBufferBarriers();
+		uploadCtx.End();
+
+		directCmdQueue->AddPendingContext(uploadCtx);
+		directCmdQueue->FlushPendingContexts();
+		directCmdQueue->NextSignalTo(uploadFence);
+		uploadFence.WaitOnCPU();
 	}
 
 	Renderer::~Renderer()
