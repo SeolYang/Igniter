d1e14c899ff3fb44e574c4a77bfb9c8e3da75632
Yang Kyowon <seolyang.contact@gmail.com>
2024-03-26 21:47:07 +0900

    로그 시간 기록 최대 '초' 까지만

--- Source/Igniter/Core/Log.cpp
+++ Source/Igniter/Core/Log.cpp
@@ -5,7 +5,7 @@ namespace ig
 {
     Logger::Logger()
     {
-        const std::string pattern = "[%Y-%m-%d %H:%M:%S.%e] [%n]  %^%v%$";
+        const std::string pattern = "[%Y-%m-%d %H:%M:%S] [%n]  %^%v%$";
         consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>(spdlog::color_mode::always);
         consoleSink->set_pattern(pattern);
         fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt", true);
