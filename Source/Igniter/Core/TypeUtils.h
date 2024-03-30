902f1dafce8105aa28772b74178e7da13e520a7b
Yang Kyowon <seolyang.contact@gmail.com>
2024-03-31 01:15:55 +0900

    Enumeration Formatting

--- Source/Igniter/Core/TypeUtils.h
+++ Source/Igniter/Core/TypeUtils.h
@@ -23,7 +23,25 @@ namespace ig
         const auto bits = static_cast<F>(flags);
         return ((bits & flag) == flag);
     }
+
 } // namespace ig
+
+template <typename Enumeration>
+    requires std::is_enum_v<Enumeration>
+struct std::formatter<Enumeration>
+{
+public:
+    constexpr auto parse(std::format_parse_context& ctx)
+    {
+        return ctx.begin();
+    }
+
+    template <typename FrameContext>
+    auto format(const Enumeration& enumerator, FrameContext& ctx) const
+    {
+        return std::format_to(ctx.out(), "{}", magic_enum::enum_name(enumerator));
+    }
+};
 
 #define IG_NUMERIC_MIN_OF(X) std::numeric_limits<std::decay_t<decltype(X)>>::min()
 #define IG_NUMERIC_MAX_OF(X) std::numeric_limits<std::decay_t<decltype(X)>>::max()
