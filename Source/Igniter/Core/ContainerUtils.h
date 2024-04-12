176526d0dd7800edafd590eb634072b2691da19d
Yang Kyowon <seolyang.contact@gmail.com>
2024-04-13 05:46:32 +0900

    Reference Wrapper Array를 간단하게 만들어주는 유틸리티 함수 추가

--- Source/Igniter/Core/ContainerUtils.h
+++ Source/Igniter/Core/ContainerUtils.h
@@ -26,4 +26,11 @@ namespace ig
         std::ranges::copy(range, std::back_inserter(newVec));
         return newVec;
     }
+
+    template <typename T, typename... Params>
+        requires (std::is_same_v<T, Params> && ...)
+    auto MakeRefArray(T& reference, [[maybe_unused]] Params&... params)
+    {
+        return std::array<Ref<T>, sizeof...(Params) + 1>{std::ref(reference), std::ref(params)... };
+    }
 } // namespace ig
