6a742f4a3f689d03a133541d1890edbc475af80e
Yang Kyowon <seolyang.contact@gmail.com>
2024-12-27 12:27:26 +0900

    Mesh Storage 구현

---
+++ Source/Igniter/Render/MeshStorage.cpp
@@ -0,0 +1,132 @@
+﻿#include "Igniter/Igniter.h"
+#include "Igniter/Render/MeshStorage.h"
+
+namespace ig
+{
+    MeshStorage::MeshStorage(RenderContext& renderContext) :
+        renderContext(renderContext),
+        staticMeshVertexGpuStorage(renderContext, "StaticMeshVertexStorage"_fs, static_cast<U32>(sizeof(VertexSM)), 8192u, false),
+        vertexIndexGpuStorage(renderContext, "VertexIndexStorage"_fs, static_cast<U32>(sizeof(U32)), 16u * 8192u, false) { }
+
+    MeshStorage::~MeshStorage()
+    {
+        for (LocalFrameIndex idx = 0; idx < NumFramesInFlight; ++idx)
+        {
+            PreRender(idx);
+        }
+    }
+
+    MeshStorage::Handle<VertexSM> MeshStorage::CreateStaticMeshVertexSpace(const U32 numVertices)
+    {
+        IG_CHECK(numVertices > 0);
+        ReadWriteLock storageMutex{staticMeshVertexSpacePackage.StorageMutex};
+
+        const Space<VertexSM> newVertexSpace{staticMeshVertexGpuStorage.Allocate(numVertices)};
+        if (!newVertexSpace.Allocation.IsValid())
+        {
+            return MeshStorage::Handle<VertexSM>{ };
+        }
+
+        return staticMeshVertexSpacePackage.Storage.Create(newVertexSpace);
+    }
+
+    MeshStorage::Handle<unsigned> MeshStorage::CreateVertexIndexSpace(const U32 numIndices)
+    {
+        IG_CHECK(numIndices > 0);
+        ReadWriteLock storageMutex{vertexIndexSpacePackage.StorageMutex};
+
+        const Space<U32> newIndexSpace{vertexIndexGpuStorage.Allocate(numIndices)};
+        if (!newIndexSpace.Allocation.IsValid())
+        {
+            return MeshStorage::Handle<U32>{ };
+        }
+
+        return vertexIndexSpacePackage.Storage.Create(newIndexSpace);
+    }
+
+    void MeshStorage::Destroy(const Handle<VertexSM> staticMeshVertexSpace)
+    {
+        IG_CHECK(currentLocalFrameIdx < NumFramesInFlight);
+        if (!staticMeshVertexSpace)
+        {
+            return;
+        }
+
+        ScopedLock packageLock{staticMeshVertexSpacePackage.StorageMutex, staticMeshVertexSpacePackage.DeferredDestroyPendingListMutex.Resources[currentLocalFrameIdx]};
+        staticMeshVertexSpacePackage.DeferredDestroyPendingList.Resources[currentLocalFrameIdx].emplace_back(staticMeshVertexSpace);
+        staticMeshVertexSpacePackage.Storage.MarkAsDestroy(staticMeshVertexSpace);
+    }
+
+    void MeshStorage::Destroy(const Handle<U32> vertexIndexSpace)
+    {
+        IG_CHECK(currentLocalFrameIdx < NumFramesInFlight);
+        if (!vertexIndexSpace)
+        {
+            return;
+        }
+
+        ScopedLock packageLock{vertexIndexSpacePackage.StorageMutex, vertexIndexSpacePackage.DeferredDestroyPendingListMutex.Resources[currentLocalFrameIdx]};
+        vertexIndexSpacePackage.DeferredDestroyPendingList.Resources[currentLocalFrameIdx].emplace_back(vertexIndexSpace);
+        vertexIndexSpacePackage.Storage.MarkAsDestroy(vertexIndexSpace);
+    }
+
+    const MeshStorage::Space<VertexSM>* MeshStorage::Lookup(const Handle<VertexSM> handle) const
+    {
+        ReadOnlyLock storageMutex{staticMeshVertexSpacePackage.StorageMutex};
+        return staticMeshVertexSpacePackage.Storage.Lookup(handle);
+    }
+
+    const MeshStorage::Space<unsigned>* MeshStorage::Lookup(const Handle<U32> handle) const
+    {
+        ReadOnlyLock storageMutex{vertexIndexSpacePackage.StorageMutex};
+        return vertexIndexSpacePackage.Storage.Lookup(handle);
+    }
+
+    void MeshStorage::PreRender(const LocalFrameIndex localFrameIdx)
+    {
+        IG_CHECK(localFrameIdx < NumFramesInFlight);
+        currentLocalFrameIdx = localFrameIdx;
+
+        {
+            ScopedLock packageLock{staticMeshVertexSpacePackage.StorageMutex, staticMeshVertexSpacePackage.DeferredDestroyPendingListMutex.Resources[currentLocalFrameIdx]};
+            for (const auto handle : staticMeshVertexSpacePackage.DeferredDestroyPendingList.Resources[currentLocalFrameIdx])
+            {
+                const Space<VertexSM>* space = staticMeshVertexSpacePackage.Storage.Lookup(handle);
+                IG_CHECK(space != nullptr);
+                staticMeshVertexGpuStorage.Deallocate(space->Allocation);
+                staticMeshVertexSpacePackage.Storage.Destroy(handle);
+            }
+            staticMeshVertexSpacePackage.DeferredDestroyPendingList.Resources[currentLocalFrameIdx].clear();
+        }
+
+        {
+            ScopedLock packageLock{vertexIndexSpacePackage.StorageMutex, vertexIndexSpacePackage.DeferredDestroyPendingListMutex.Resources[currentLocalFrameIdx]};
+            for (const auto handle : vertexIndexSpacePackage.DeferredDestroyPendingList.Resources[currentLocalFrameIdx])
+            {
+                const Space<U32>* space = vertexIndexSpacePackage.Storage.Lookup(handle);
+                IG_CHECK(space != nullptr);
+                vertexIndexGpuStorage.Deallocate(space->Allocation);
+                vertexIndexSpacePackage.Storage.Destroy(handle);
+            }
+            vertexIndexSpacePackage.DeferredDestroyPendingList.Resources[currentLocalFrameIdx].clear();
+        }
+    }
+
+    MeshStorage::Statistics MeshStorage::GetStatistics() const
+    {
+        Statistics stats{};
+        {
+            ReadOnlyLock storageMutex{staticMeshVertexSpacePackage.StorageMutex};
+            stats.StaticMeshVertexStorageUsage = staticMeshVertexGpuStorage.GetAllocatedSize();
+            stats.StaticMeshVertexStorageSize = staticMeshVertexGpuStorage.GetBufferSize();
+        }
+
+        {
+            ReadOnlyLock storageMutex{vertexIndexSpacePackage.StorageMutex};
+            stats.VertexIndexStorageUsage = vertexIndexGpuStorage.GetAllocatedSize();
+            stats.VertexIndexStorageSize = vertexIndexGpuStorage.GetBufferSize();
+        }
+
+        return stats;
+    }
+}
---
+++ Source/Igniter/Render/MeshStorage.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include "Igniter/Igniter.h"
+#include "Igniter/Core/Handle.h"
+#include "Igniter/Render/Common.h"
+#include "Igniter/Render/GpuStorage.h"
+#include "Igniter/Render/Vertex.h"
+
+namespace ig
+{
+    class RenderContext;
+
+    class MeshStorage final
+    {
+    public:
+        template <typename Ty>
+        struct Space
+        {
+            GpuStorage::Allocation Allocation;
+        };
+
+        template <typename Ty>
+        using Handle = Handle<Space<Ty>, MeshStorage>;
+
+        struct Statistics
+        {
+            Size StaticMeshVertexStorageUsage;
+            Size StaticMeshVertexStorageSize;
+            Size VertexIndexStorageUsage;
+            Size VertexIndexStorageSize;
+        };
+
+    public:
+        explicit MeshStorage(RenderContext& renderContext);
+        MeshStorage(const MeshStorage&) = delete;
+        MeshStorage(MeshStorage&&)      = delete;
+        ~MeshStorage();
+
+        MeshStorage& operator=(const MeshStorage&) = delete;
+        MeshStorage& operator=(MeshStorage&&)      = delete;
+
+        Handle<VertexSM> CreateStaticMeshVertexSpace(const U32 numVertices);
+        Handle<U32>      CreateVertexIndexSpace(const U32 numIndices);
+
+        void Destroy(const Handle<VertexSM> staticMeshVertexSpace);
+        void Destroy(const Handle<U32> vertexIndexSpace);
+
+        [[nodiscard]] const Space<VertexSM>* Lookup(const Handle<VertexSM> handle) const;
+        [[nodiscard]] const Space<U32>*      Lookup(const Handle<U32> handle) const;
+
+        void PreRender(const LocalFrameIndex localFrameIdx);
+
+        [[nodiscard]] RenderHandle<GpuBuffer> GetStaticMeshVertexStorageBuffer() const noexcept { return staticMeshVertexGpuStorage.GetGpuBuffer(); }
+        [[nodiscard]] RenderHandle<GpuView>   GetStaticMeshVertexStorageShaderResourceView() const noexcept { return staticMeshVertexGpuStorage.GetShaderResourceView(); }
+        [[nodiscard]] GpuFence&               GetStaticMeshVertexStorageFence() noexcept { return staticMeshVertexGpuStorage.GetStorageFence(); }
+        [[nodiscard]] RenderHandle<GpuBuffer> GetVertexIndexStorageBuffer() const noexcept { return vertexIndexGpuStorage.GetGpuBuffer(); }
+        [[nodiscard]] RenderHandle<GpuView>   GetVertexIndexStorageShaderResourceView() const noexcept { return vertexIndexGpuStorage.GetShaderResourceView(); }
+        [[nodiscard]] GpuFence&               GetVertexIndexStorageFence() noexcept { return vertexIndexGpuStorage.GetStorageFence(); }
+
+        [[nodiscard]] Statistics GetStatistics() const;
+
+    private:
+        LocalFrameIndex                                             currentLocalFrameIdx = 0;
+        RenderContext&                                              renderContext;
+        DeferredResourceManagePackage<Space<VertexSM>, MeshStorage> staticMeshVertexSpacePackage;
+        GpuStorage                                                  staticMeshVertexGpuStorage;
+        DeferredResourceManagePackage<Space<U32>, MeshStorage>      vertexIndexSpacePackage;
+        GpuStorage                                                  vertexIndexGpuStorage;
+    };
+}
