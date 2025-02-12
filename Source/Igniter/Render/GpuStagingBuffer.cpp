092f63f2fc094faceeecf30f100bdfbc5372a257
Yang Kyowon <seolyang.contact@gmail.com>
2025-02-12 22:32:33 +0900

    GpuStagingBuffer 유틸리티 클래스 추가

---
+++ Source/Igniter/Render/GpuStagingBuffer.cpp
@@ -0,0 +1,42 @@
+﻿#include "Igniter/Igniter.h"
+#include "Igniter/D3D12/GpuBuffer.h"
+#include "Igniter/D3D12/GpuBufferDesc.h"
+#include "Igniter/Render/RenderContext.h"
+#include "Igniter/Render/GpuStagingBuffer.h"
+
+namespace ig
+{
+    GpuStagingBuffer::GpuStagingBuffer(RenderContext& renderCtx, const GpuStagingBufferDesc& desc)
+        : renderCtx(&renderCtx)
+    {
+        IG_CHECK(desc.BufferSize < 0xFFFFFFFFUi32);
+        
+        GpuBufferDesc bufferDesc;
+        bufferDesc.AsUploadBuffer((U32)desc.BufferSize, false);
+        for (const LocalFrameIndex localFrameIdx : LocalFramesView)
+        {
+            bufferDesc.DebugName = String(std::format("{} ({})", desc.DebugName, localFrameIdx));
+            buffer[localFrameIdx] = renderCtx.CreateBuffer(bufferDesc);
+            mappedBuffer[localFrameIdx] = renderCtx.Lookup(buffer[localFrameIdx])->Map();
+        }
+    }
+
+    GpuStagingBuffer::~GpuStagingBuffer()
+    {
+        if (renderCtx == nullptr)
+        {
+            IG_CHECK(!buffer);
+            return;
+        }
+
+        for (const LocalFrameIndex localFrameIdx : LocalFramesView)
+        {
+            if (GpuBuffer* bufferPtr = renderCtx->Lookup(buffer[localFrameIdx]);
+                bufferPtr != nullptr)
+            {
+                bufferPtr->Unmap();
+            }
+            renderCtx->DestroyBuffer(buffer[localFrameIdx]);
+        }
+    }
+}
---
+++ Source/Igniter/Render/GpuStagingBuffer.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "Igniter/Render/Common.h"
+
+namespace ig
+{
+    enum class EGpuStagingBufferFlags : U8
+    {
+        None = 0,
+    };
+
+    IG_ENUM_FLAGS(EGpuStagingBufferFlags);
+
+    struct GpuStagingBufferDesc
+    {
+        Bytes BufferSize = 0; // Assert(>0)
+        EGpuStagingBufferFlags Flags = EGpuStagingBufferFlags::None;
+        String DebugName = "UnnamedStagingBuffer"_fs;
+    };
+
+    class GpuBuffer;
+    class RenderContext;
+
+    class GpuStagingBuffer
+    {
+    public:
+        GpuStagingBuffer(RenderContext& renderCtx, const GpuStagingBufferDesc& desc);
+        GpuStagingBuffer(const GpuStagingBuffer&) = delete;
+        GpuStagingBuffer(GpuStagingBuffer&&) noexcept = delete;
+        ~GpuStagingBuffer();
+
+        GpuStagingBuffer& operator=(const GpuStagingBuffer&) = delete;
+        GpuStagingBuffer& operator=(GpuStagingBuffer&&) noexcept = delete;
+
+        [[nodiscard]] Handle<GpuBuffer> GetBuffer(const LocalFrameIndex localFrameIdx) const noexcept { return buffer[localFrameIdx]; }
+        [[nodiscard]] U8* GetMappedBuffer(const LocalFrameIndex localFrameIdx) noexcept { return mappedBuffer[localFrameIdx]; }
+
+    private:
+        RenderContext* renderCtx = nullptr;
+        InFlightFramesResource<Handle<GpuBuffer>> buffer;
+        InFlightFramesResource<U8*> mappedBuffer;
+    };
+}
